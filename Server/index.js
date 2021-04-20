46cc70daca2e5c9471472e436afb4a3d6e9a3ab8
Benjamin-Entropik <benjamin@entropik.digital>
2021-04-20 09:52:36 +0400

    mise en place du login parti back [Benj]

--- Server/index.js
+++ Server/index.js
@@ -1,23 +1,24 @@
 // Import package
 const express = require('express')
+const path = require('path');
 require('dotenv').config()
-
+// const TokenAuth = require('../services/JWToken.js').AuthToken
+const cors=require("cors");
 // Import Route
 const AuthRoute = require('./routes/Auth.js')
 const UserRoute = require('./routes/UserRoutes.js')
 const PictureRoute = require('./routes/PicturesRoute.js')
+const headerApi = require('./configApi');
 
-// Uncomment next line to make table migration
-//  require('./models/model_instant.js')
-
-// Express config
-const app = express()
-app.use(express.json())
-
+var app = express()
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(headerApi); 
 // Use routes in app
 app.use(AuthRoute);
 app.use(UserRoute);
 app.use(PictureRoute)
+
 
 
 app.listen(8080, () => {
